fcc98c52c806f2d8968d580420583aa500487cb6
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 00:51:24 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-// Load modules .
-const express = require('express');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
 const exphbs = require('express-handlebars');
-const mongoose = require('mongoose');
-const passport = require('passport');
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
 const Handlebars = require('handlebars');
-const methodOverride = require('method-override');
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
-
-// Connect to MongoURI exported from external file.
+const mongoose = require('mongoose');
+const passport = require('passport');
+const express = require('express');
 const keys = require('./config/keys.js');
-// Load models.
 const User = require('./models/user.js');
 const Post = require('./models/post');
-// Link passports to the server.
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+
 require('./passport/google-passport');
 require('./passport/facebook-passport');
-// Link helpers.
+
 const {
     ensureAuthentication,
     ensureGuest
 } = require('./helpers/auth');
-// Initialize our application.
+
 const app = express();
 
 app.engine('handlebars', exphbs({
@@ -31,8 +27,6 @@ app.engine('handlebars', exphbs({
     handlebars: allowInsecurePrototypeAccess(Handlebars)
 }));
 app.set('view engine', 'handlebars');
-
-// Express config.
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ 
     extended: false 
@@ -46,23 +40,17 @@ app.use(session({
 app.use(methodOverride('_method'));
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Golbal variables for user.
 app.use((req, res, next) => {
     res.locals.user = req.user || null;
     next();
 });
-
-// Setup template engine for the view.
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
-
 app.set('view engine', 'handlebars');
-// Setup static files to serve css javascript and images.
 app.use(express.static('public'));
+
 mongoose.Promise = global.Promise;
-// connect to remote database.
 mongoose.connect(keys.MongoURI, {
     useUnifiedTopology: true,
     useNewUrlParser: true
@@ -73,10 +61,8 @@ mongoose.connect(keys.MongoURI, {
     console.log(err);
 });
 
-// Set environment variable for port.
 const port = process.env.PORT || 3000;
 
-// Handle routes.
 app.get('/', ensureGuest, (req, res) => {
     res.render('home');
 });
@@ -85,7 +71,6 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// Google auth route.
 app.get('/auth/google',
   passport.authenticate('google', { 
     scope: ['profile', 'email'] 
@@ -96,11 +81,9 @@ app.get('/auth/google/callback',
     failureRedirect: '/login'
  }),
   (req, res) => {
-    // Successful authentication, redirect home.
     res.redirect('/profile');
   });
 
-// Facebook auth route.
 app.get('/auth/facebook',
   passport.authenticate('facebook',{
       scope: 'email'
@@ -111,11 +94,9 @@ app.get('/auth/facebook/callback',
       failureRedirect: '/' 
   }),
   (req, res) => {
-    // Successful authentication, redirect home.
     res.redirect('/profile');
   });
 
-// Handdle profile route.
 app.get('/profile', ensureAuthentication, (req, res) => {
     Post.find({user: req.user._id})
     .populate('user')
@@ -126,7 +107,7 @@ app.get('/profile', ensureAuthentication, (req, res) => {
         });
     }); 
 });
-// Handle route for ALL USERS.
+
 app.get('/users', ensureAuthentication, (req, res) => {
     User.find({}).then((users) =>{
         res.render('users', {
@@ -135,7 +116,6 @@ app.get('/users', ensureAuthentication, (req, res) => {
     });
 });
 
-// Display one USER profile.
 app.get('/user/:id', ensureAuthentication, (req, res) => {
     User.findById({_id: req.params.id})
     .then((user) => {
@@ -145,7 +125,6 @@ app.get('/user/:id', ensureAuthentication, (req, res) => {
     });
 });
 
-// Handle email POST route.
 app.post('/addEmail', ensureAuthentication, (req, res) => {
     const email = req.body.email;
     User.findById({_id: req.user._id})
@@ -157,7 +136,7 @@ app.post('/addEmail', ensureAuthentication, (req, res) => {
         })
     })
 })
-// Handle phone route.
+
 app.post('/addPhone', ensureAuthentication, (req, res) => {
     const phone = req.body.phone;
     User.findById({_id: req.user._id})
@@ -170,7 +149,6 @@ app.post('/addPhone', ensureAuthentication, (req, res) => {
     });
 });
 
-// Handle location POST route.
 app.post('/addLocation', ensureAuthentication, (req, res) => {
     const location = req.body.location;
     User.findById({_id: req.user._id})
@@ -183,16 +161,18 @@ app.post('/addLocation', ensureAuthentication, (req, res) => {
     });
 });
 
-// Handle get routes for POST.
 app.get('/addPost', ensureAuthentication, (req, res) => {
     res.render('addPost');
 });
-// Handle post route.
+
 app.post('/savePost', ensureAuthentication, (req, res) => {
     var allowComments;
-    if(req.body.allowComments){
+    if(req.body.allowComments)
+    {
         allowComments = true;
-    }else{
+    }
+    else
+    {
         allowComments = false;
     }
     const newPost = {
@@ -207,7 +187,7 @@ app.post('/savePost', ensureAuthentication, (req, res) => {
         res.redirect('/posts');
     });
 });
-// Handle EDIT POST route.
+
 app.get('/editPost/:id', ensureAuthentication, (req, res) => {
     Post.findOne({_id: req.params.id})
     .then((post) => {
@@ -216,7 +196,7 @@ app.get('/editPost/:id', ensureAuthentication, (req, res) => {
         });
     });
 });
-// Handle PUT route to SAVE EDITED POST.
+
 app.put('/editingPost/:id', ensureAuthentication, (req, res) => {
     Post.findOne({_id: req.params.id})
     .then((post) => {
@@ -236,14 +216,14 @@ app.put('/editingPost/:id', ensureAuthentication, (req, res) => {
         });
     });
 });
-// Handle DELETE route.
+
 app.delete('/:id', ensureAuthentication, (req, res) => {
     Post.remove({_id: req.params.id})
     .then(() => {
         res.redirect('profile');
     })
 })
-// Handle posts route
+
 app.get('/posts', ensureAuthentication, (req, res) => {
     Post.find({status: 'public'})
     .populate('user')
@@ -255,7 +235,7 @@ app.get('/posts', ensureAuthentication, (req, res) => {
         });
     });
 });
-// Handle display single users all public posts.
+
 app.get('/showposts/:id', ensureAuthentication, (req, res) => {
     Post.find({user: req.params.id, status: 'public'})
     .populate('user')
@@ -266,7 +246,7 @@ app.get('/showposts/:id', ensureAuthentication, (req, res) => {
         });
     });
 });
-// Handle comments save to database
+
 app.post('/addComment/:id', ensureAuthentication, (req, res) => {
     Post.findOne({_id: req.params.id})
     .then((post) => {
@@ -281,11 +261,12 @@ app.post('/addComment/:id', ensureAuthentication, (req, res) => {
         })
     });
 });
-// User logout handle route.
+
 app.get('/logout', (req, res) => {
     req.logOut();
     res.redirect('/');
 });
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
